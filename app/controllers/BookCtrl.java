a517f8cb5b4f87c4c8fc9329981faff4ba8bb4f7
ctardella <christophe.tardella@gmail.com>
2011-11-22 13:13:01 +0100

    Add page 'Collecter BD'

--- app/controllers/BookCtrl.java
+++ app/controllers/BookCtrl.java
@@ -3,10 +3,13 @@ package controllers;
 import java.util.List;
 
 import models.Book;
+import models.OwnedBook;
 import models.Serie;
 import play.data.validation.Valid;
 import play.data.validation.Validation;
 import play.i18n.Messages;
+
+import com.kebuu.JPQLHelper;
 
 @Check("book-manager")
 public class BookCtrl extends LoggedApplication {
@@ -36,5 +39,26 @@ public class BookCtrl extends LoggedApplication {
 		flash.put("message", "La BD a été ajoutée, vous pouvez créer à nouveau.");
 		
 		BookCtrl.prepareAdd();
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void prepareCollect() {
+		List<Book> connectedUserBooks = 
+			Book.find("select book from OwnedBook ownedBook " +
+					"join ownedBook.baseBook book " +
+					"where ownedBook.owner.id = ?", getConnectedUser().id).fetch();
+		
+		List<Book> availableBooks = (List<Book>) new JPQLHelper("select book from Book book")
+		.addNotInCondition("book", "connectedUserBooks", connectedUserBooks)
+		.addOrderByCondition("book.number").fetch();
+		
+		render(availableBooks);
+	}
+	
+	public static void collectBd(final Book book) {
+		OwnedBook ownedBook = new OwnedBook(book, getConnectedUser());
+		ownedBook.create();
+		
+		prepareCollect();
 	}
 }
