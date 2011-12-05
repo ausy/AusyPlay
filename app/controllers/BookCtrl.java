0fa58c0f31b7d6d2ca39023efe8ed4915e6f4290
Bashar Burzmali <quizzach.adderach@free.fr>
2011-12-05 12:39:50 +0100

    travail en cours sur la validation ajout de transition sur l'écran

--- app/controllers/BookCtrl.java
+++ app/controllers/BookCtrl.java
@@ -21,49 +21,60 @@ public class BookCtrl extends LoggedApplication {
 	}
 
 	public static void addBook(final @Valid Book book) {
+		Validation.required("book.serie.name", book.serie.name);
+
 		if (Validation.hasErrors()) {
-			
+			for (play.data.validation.Error error : Validation.errors()) {
+				System.out.println(error.message() + "  " + error.getKey());
+			}
+
 			// Specific treatment for isbn, just to provide example
-			if(!Validation.errors("book.isbn").isEmpty()) {
+			if (!Validation.errors("book.isbn").isEmpty()) {
 				flash.put("error_isbn", Messages.get("error.book.isbn.msg"));
 			}
-			
+
 			params.flash(); // add http parameters to the flash scope
 			Validation.keep(); // keep the errors for the next request
 			prepareAdd();
 		}
-		
+
 		if (book.serie.id <= 0) {
 			book.serie = null;
 		}
 		book.create();
-		
-		//Send WebSocket message
-		WebSocket.liveStream.publish(MessageFormat.format("La BD ''{0}'' a été ajoutée dans la série ''{1}''", book.title, book.serie.name));
-		
-		flash.put("message", "La BD a été ajoutée, vous pouvez créer à nouveau.");
-		
+
+		// Send WebSocket message
+		WebSocket.liveStream.publish(MessageFormat.format(
+				"La BD ''{0}'' a été ajoutée dans la série ''{1}''",
+				book.title, book.serie.name));
+
+		flash.put("message",
+				"La BD a été ajoutée, vous pouvez créer à nouveau.");
+
 		BookCtrl.prepareAdd();
 	}
 
 	@SuppressWarnings("unchecked")
 	public static void prepareCollect() {
-		List<Book> connectedUserBooks = 
-			Book.find("select book from OwnedBook ownedBook " +
-					"join ownedBook.baseBook book " +
-					"where ownedBook.owner.id = ?", getConnectedUser().id).fetch();
-		
-		List<Book> availableBooks = (List<Book>) new JPQLHelper("select book from Book book")
-		.addNotInCondition("book", "connectedUserBooks", connectedUserBooks)
-		.addOrderByCondition("book.number").fetch();
-		
+		List<Book> connectedUserBooks = Book
+				.find("select book from OwnedBook ownedBook "
+						+ "join ownedBook.baseBook book "
+						+ "where ownedBook.owner.id = ?", getConnectedUser().id)
+				.fetch();
+
+		List<Book> availableBooks = (List<Book>) new JPQLHelper(
+				"select book from Book book")
+				.addNotInCondition("book", "connectedUserBooks",
+						connectedUserBooks).addOrderByCondition("book.number")
+				.fetch();
+
 		render(availableBooks);
 	}
-	
+
 	public static void collectBd(final Book book) {
 		OwnedBook ownedBook = new OwnedBook(book, getConnectedUser());
 		ownedBook.create();
-		
+
 		prepareCollect();
 	}
 }
